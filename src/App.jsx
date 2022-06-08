4d0506f018f0e84130216d5a6b488f9eebd590bc
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-06-08 19:56:05 +0200

    add cart page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
     <div>
-      <Product />
+      <Cart />
     </div>
   );
 };
